85154b5526c0909dd6809d184e6286aca9a446e3
KTECH\Cihan <c.tekin@keesingtechnologies.com>
2023-01-04 21:05:19 +0100

    Singleton instance

--- singleton-pattern/Program.cs
+++ singleton-pattern/Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using singleton_pattern;
+
 Console.WriteLine("Hello, World!");
+
+
+Thread process1 = new(() =>
+{
+    TestSingleton("Process1");
+});
+
+Thread process2 = new(() =>
+{
+    TestSingleton("Process2");
+});
+
+process1.Start();
+process2.Start();
+process1.Join();
+process2.Join();
+
+static void TestSingleton(string value)
+{
+    Singleton singleton = Singleton.GetInstance(value);
+    Console.Write(singleton.Value);
+}
---
+++ singleton-pattern/Singleton.cs
@@ -0,0 +1,32 @@
+﻿namespace singleton_pattern
+{
+    public sealed class Singleton
+    {
+        private Singleton()
+        {
+
+        }
+
+        private static Singleton? _instance;
+
+        private static readonly object _lock = new();
+
+        public static Singleton GetInstance(string value)
+        {
+            if (_instance == null)
+            {
+                lock (_lock)
+                {
+                    _instance ??= new Singleton
+                        {
+                            Value = value
+                        };
+                }
+            }
+
+            return _instance;
+        }
+
+        public string Value { get; set; } = new ("");
+    }
+}
